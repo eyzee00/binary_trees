b4a436f60da8dcbbeeea4b77545a6d631c4692e6
eyzee00 <azizihamza1910@gmail.com>
2024-01-31 21:27:00 +0100

    !

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -8,12 +8,20 @@
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t leftHeight = 1, rightHeight = 1;
+	size_t leftHeight = 0, rightHeight = 0;
 
 	if (tree == NULL)
 		return (0);
-	leftHeight += binary_tree_height(tree->left);
-	rightHeight += binary_tree_height(tree->right);
+
+	if (tree->left != NULL)
+		leftHeight = binary_tree_height(tree->left) + 1;
+	else
+		leftHeight = 0;
+
+	if (tree->right != NULL)
+		rightHeight = binary_tree_height(tree->right) + 1;
+	else
+		rightHeight = 0;
 
 	if (leftHeight > rightHeight)
 		return leftHeight;
