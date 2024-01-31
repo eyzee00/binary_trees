49de4da125482aba83d1e4e96ba71c9564195743
eyzee00 <azizihamza1910@gmail.com>
2024-02-01 00:39:19 +0100

    !

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -10,14 +10,13 @@ int binary_tree_is_full(const binary_tree_t *tree)
 {
 	if (tree == NULL)
 		return (0);
-	return (1);
+	return (is_full(tree));
 }
 
 /**
- * is_full_recursive - Checks if a binary tree is full
+ * is_full - checks if a binary tree is full
  * @tree: A pointer to the root node of the binary tree
- * Return: If tree is not full, 0.
- *         Otherwise, 1.
+ * Return: If tree is not full 0, 1 otherwise
  */
 int is_full(const binary_tree_t *tree)
 {
